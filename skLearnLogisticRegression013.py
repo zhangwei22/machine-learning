0f8feae52fe819c979a8f186425b0568b3399f3c
zhangwei <928241517@qq.com>
2017-11-19 10:47:46 +0800

    add some model code

--- skLearnLogisticRegression013.py
+++ skLearnLogisticRegression013.py
@@ -89,3 +89,21 @@ plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.show()
+
+lr.predict_proba(X_test_std[0, :])
+
+weights, params = [], []
+for c in np.arange(-5, 5):
+    lr = LogisticRegression(C=10 ** c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10 ** c)
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0], label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--', label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
+plt.show()
