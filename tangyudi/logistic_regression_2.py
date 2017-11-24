310c36bb0911d2af9c4bb783a9cb590d75545d1f
zhangwei <928241517@qq.com>
2017-11-24 13:50:47 +0800

    fix

--- tangyudi/logistic_regression_2.py
+++ tangyudi/logistic_regression_2.py
@@ -10,3 +10,13 @@ class LogisticRegression:
         print(X.shape)
         X_ = np.linalg.inv(X.T.dot(X))
         self.w = X_.dot(X.T).dot(y)
+
+
+# world_alcohol = np.genfromtxt("world_alcohol.txt", delimiter=",", dtype=str)
+# print(type(world_alcohol))
+# print(world_alcohol)
+# print(help(np.genfromtxt))
+vector = np.array([5, 10, 15, 20])
+print(vector)
+matrix = np.array([[5, 10, 15], [20, 25, 30], [35, 40, 45]])
+print(matrix)
