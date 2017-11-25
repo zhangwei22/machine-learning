0b73a1db5d79533f1242bec904467d5b0eade488
zhangwei <928241517@qq.com>
2017-11-25 11:25:51 +0800

    kNN

---
+++ action/kNN_2.py
@@ -0,0 +1,41 @@
+from numpy import *
+import operator
+
+'''
+k-近邻算法
+原理:存在一个样本数据集合，也称作训练样本集，并且样本集中每个数据都存在标签，即我们知道样本集中每一数据与所属分类的对应关系。
+输入没有标签的新数据后，将新数据的每个特征与样本集中数据对应的特征进行比较，然后算法提取样本集中特征最相似数据（最近邻）的分类标签。
+一般来说，我们只选择样本数据集中前k个最相似的数据，这就是k-近邻算法中k的出处，通常k是不大于20的整数。
+最后，选择k个最相似数据中出现次数最多的分类，作为新数据的分类。
+'''
+
+
+# 创建训练数据
+def createDataSet():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+# k-近邻算法
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat ** 2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+if __name__ == '__main__':
+    group, labels = createDataSet()
+    print(group)
+    print(labels)
+    result = classify0([0, 0], group, labels, 3)
+    print(result)
