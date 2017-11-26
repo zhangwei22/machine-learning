b6fbfdbe1638a17bf0ad6449001f4eaec6b9362f
zhangwei <928241517@qq.com>
2017-11-26 09:54:25 +0800

    kNN iterate

--- action/kNN_2.py
+++ action/kNN_2.py
@@ -1,5 +1,7 @@
 from numpy import *
 import operator
+import matplotlib
+import matplotlib.pyplot as plt
 
 '''
 k-近邻算法
@@ -10,8 +12,8 @@ k-近邻算法
 '''
 
 
-# 创建训练数据
 def createDataSet():
+    # 创建训练数据
     group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
     labels = ['A', 'A', 'B', 'B']
     return group, labels
@@ -20,17 +22,68 @@ def createDataSet():
 # k-近邻算法
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
+    # 距离计算
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances ** 0.5
     sortedDistIndicies = distances.argsort()
     classCount = {}
+    # 选择距离最小的k个点
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    # 排序
     sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
+
+
+def file2matrix(filename):
+    fr = open(filename)
+    # 得到文件行数
+    arrayOlines = fr.readlines()
+    numberOfLines = len(arrayOlines)
+    # 创建返回的numpy矩阵
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    # 解析文件数据到列表
+    for line in arrayOlines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0: 3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
+# 归一化特征值
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+# 测试
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix(
+        '/Users/zhangwei/Desktop/python-machine-learn/machinelearninginaction/Ch02/datingTestSet.txt')
+    normMat, ranges, minvals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m, 3])
+        print("the classifier came back with:%d, the real answer is:%d", classifierResult, datingLabels[i])
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print("the total error rate is:%f", errorCount / float(numTestVecs))
 
 
 if __name__ == '__main__':
@@ -39,3 +92,22 @@ if __name__ == '__main__':
     print(labels)
     result = classify0([0, 0], group, labels, 3)
     print(result)
+
+    datingDataMat, datingLabels = file2matrix(
+        '/Users/zhangwei/Desktop/python-machine-learn/machinelearninginaction/Ch02/datingTestSet2.txt')
+    print(datingDataMat)
+    print(datingLabels)
+
+    # 构建散点图
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111)
+    # ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0 * array(datingLabels), 15.0 * array(datingLabels))
+    # plt.show()
+
+    # 归一化特征值
+    normMat, ranges, minvals = autoNorm(datingDataMat)
+    print(normMat)
+    print(ranges)
+    print(minvals)
+
+    datingClassTest()
