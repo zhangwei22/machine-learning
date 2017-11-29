a5084119322c8b6b75c914be34d7bb443b568620
zhangwei <928241517@qq.com>
2017-11-29 09:18:18 +0800

    classify by random gradient rise

--- machine_learning_in_action/logisticRegres_5.py
+++ machine_learning_in_action/logisticRegres_5.py
@@ -75,10 +75,101 @@ def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     return weights
 
 
-if __name__ == '__main__':
-    dataArr, labelMat = loadDataSet()
-    weights = gradAscent(dataArr, labelMat)
-    plotBestFit(weights.getA())
+#
+# def classifyVector(inX, weights):
+#     print(sum(inX * weights))
+#     prob = sigmoid(sum(inX * weights))
+#     if prob > 0.5:
+#         return 1.0
+#     else:
+#         return 0.0
+#
+#
+# def colicTest():
+#     frTrain = open('/Users/zhangwei/Desktop/python-machine-learn/machinelearninginaction/Ch05/horseColicTraining.txt')
+#     frTest = open('/Users/zhangwei/Desktop/python-machine-learn/machinelearninginaction/Ch05/horseColicTest.txt')
+#     trainingSet = []
+#     trainingLabels = []
+#     for line in frTrain.readlines():
+#         currLine = line.strip().split('\t')
+#         lineArr = []
+#         for i in range(2):
+#             lineArr.append(float(currLine[i]))
+#         trainingSet.append(lineArr)
+#         trainingLabels.append(float(currLine[21]))
+#     trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 500)
+#     errorCount = 0
+#     numTestVec = 0.0
+#     for line in frTest.readlines():
+#         numTestVec += 1.0
+#         currLine = line.strip().split('\t')
+#         lineArr = []
+#         for i in range(21):
+#             lineArr.append(float(currLine[i]))
+#         if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+#             errorCount += 1
+#     errorRate = (float(errorCount) / numTestVec)
+#     print("the error rate of this test is:%f" % errorRate)
+#     return errorRate
+#
+#
+# def multiTest():
+#     numTests = 10
+#     errorSum = 0.0
+#     for k in range(numTests):
+#         errorSum += colicTest()
+#     print("after%d iterations the average error rate is:%f" % (numTests, errorSum / float(numTests)))
 
-    weights = stocGradAscent1(array(dataArr), labelMat, numIter=500)
-    plotBestFit(weights)
+def classifyVector(inX, weights):
+    prob = sigmoid(sum(inX * weights))
+    if prob > 0.5:
+        return 1.0
+    else:
+        return 0.0
+
+
+def colicTest():
+    frTrain = open('/Users/zhangwei/Desktop/python-machine-learn/machinelearninginaction/Ch05/horseColicTraining.txt')
+    frTest = open('/Users/zhangwei/Desktop/python-machine-learn/machinelearninginaction/Ch05/horseColicTest.txt')
+    trainingSet = []
+    trainingLabels = []
+    for line in frTrain.readlines():
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        trainingSet.append(lineArr)
+        trainingLabels.append(float(currLine[21]))
+    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 1000)
+    errorCount = 0
+    numTestVec = 0.0
+    for line in frTest.readlines():
+        numTestVec += 1.0
+        currLine = line.strip().split('\t')
+        lineArr = []
+        for i in range(21):
+            lineArr.append(float(currLine[i]))
+        if int(classifyVector(array(lineArr), trainWeights)) != int(currLine[21]):
+            errorCount += 1
+    errorRate = (float(errorCount) / numTestVec)
+    print("the error rate of this test is: %f" % errorRate)
+    return errorRate
+
+
+def multiTest():
+    numTests = 10
+    errorSum = 0.0
+    for k in range(numTests):
+        errorSum += colicTest()
+    print("after %d iterations the average error rate is: %f" % (numTests, errorSum / float(numTests)))
+
+
+if __name__ == '__main__':
+    # dataArr, labelMat = loadDataSet()
+    # weights = gradAscent(dataArr, labelMat)
+    # plotBestFit(weights.getA())
+    #
+    # weights = stocGradAscent1(array(dataArr), labelMat, numIter=500)
+    # plotBestFit(weights)
+
+    multiTest()
