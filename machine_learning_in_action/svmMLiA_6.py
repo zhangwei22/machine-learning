1ee58ef933499ed9652805f6ef1a2501204f852e
zhangwei <928241517@qq.com>
2017-12-04 09:12:27 +0800

    renamed source code and added a svm simple code

---
+++ machine_learning_in_action/svmMLiA_6.py
@@ -0,0 +1,53 @@
+from numpy import *
+
+import random
+
+
+def loadData(filename):
+    dataMat = []
+    labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat, labelMat
+
+
+def selectJrand(i, m):
+    j = i
+    while (j == i):
+        j = int(random.uniform(0, m))
+    return j
+
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+
+def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    b = 0
+    m, n = shape(dataMatrix)
+    alphas = mat(zeros((m, 1)))
+    iter = 0
+    while (iter < maxIter):
+        alphaPairsChanged = 0
+        for i in range(m):
+            fXi = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[i, :].T)) + b
+            Ei = fXi - float(labelMat[i])
+            if ((labelMat[i] * Ei < -toler) and (alphas[i] < C)) or ((labelMat[i] * Ei > toler) and (alphas[i] > 0)):
+                j = selectJrand(i, m)
+                fXj = float(multiply(alphas, labelMat).T * (dataMatrix * dataMatrix[j, :].T))
+
+
+
+if __name__ == '__main__':
+    dataArr, labelArr = loadData('source_code/Ch06/testSet.txt')
+    print(dataArr)
+    print(labelArr)
