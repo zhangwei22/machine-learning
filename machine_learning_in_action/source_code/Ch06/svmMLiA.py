150b4ebe22d7bec2a2a632d0a39ec28c1b9c9019
zhangwei <928241517@qq.com>
2017-12-05 09:12:48 +0800

    modify print

--- machine_learning_in_action/source_code/Ch06/svmMLiA.py
+++ machine_learning_in_action/source_code/Ch06/svmMLiA.py
@@ -49,12 +49,12 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 else:
                     L = max(0, alphas[j] + alphas[i] - C)
                     H = min(C, alphas[j] + alphas[i])
-                if L==H: print "L==H"; continue
+                if L==H: print("L==H"); continue
                 eta = 2.0 * dataMatrix[i,:]*dataMatrix[j,:].T - dataMatrix[i,:]*dataMatrix[i,:].T - dataMatrix[j,:]*dataMatrix[j,:].T
-                if eta >= 0: print "eta>=0"; continue
+                if eta >= 0: print("eta>=0"); continue
                 alphas[j] -= labelMat[j]*(Ei - Ej)/eta
                 alphas[j] = clipAlpha(alphas[j],H,L)
-                if (abs(alphas[j] - alphaJold) < 0.00001): print "j not moving enough"; continue
+                if (abs(alphas[j] - alphaJold) < 0.00001): print("j not moving enough"); continue
                 alphas[i] += labelMat[j]*labelMat[i]*(alphaJold - alphas[j])#update i by the same amount as j
                                                                         #the update is in the oppostie direction
                 b1 = b - Ei- labelMat[i]*(alphas[i]-alphaIold)*dataMatrix[i,:]*dataMatrix[i,:].T - labelMat[j]*(alphas[j]-alphaJold)*dataMatrix[i,:]*dataMatrix[j,:].T
@@ -63,10 +63,10 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
                 elif (0 < alphas[j]) and (C > alphas[j]): b = b2
                 else: b = (b1 + b2)/2.0
                 alphaPairsChanged += 1
-                print "iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged)
+                print("iter: %d i:%d, pairs changed %d" % (iter,i,alphaPairsChanged))
         if (alphaPairsChanged == 0): iter += 1
         else: iter = 0
-        print "iteration number: %d" % iter
+        print("iteration number: %d" % iter)
     return b,alphas
 
 def kernelTrans(X, A, kTup): #calc the kernel or transform data to a higher dimensional space
