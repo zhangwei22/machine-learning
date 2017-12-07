3dc0c2387c35d2aa95fd2b556d0007335d207c7a
zhangwei <928241517@qq.com>
2017-12-07 09:46:24 +0800

    ada boost train decision stump

--- machine_learning_in_action/adaBoost_7.py
+++ machine_learning_in_action/adaBoost_7.py
@@ -46,7 +46,103 @@ def buildStump(dataArr, classLabels, D):
     return bestStump, minError, bestClasEst
 
 
+def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m, 1)) / m)  # init D to all equal
+    aggClassEst = mat(zeros((m, 1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)  # build Stump
+        # print "D:",D.T
+        alpha = float(
+            0.5 * log((1.0 - error) / max(error, 1e-16)))  # calc alpha, throw in max(error,eps) to account for error=0
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)  # store Stump Params in Array
+        # print "classEst: ",classEst.T
+        expon = multiply(-1 * alpha * mat(classLabels).T, classEst)  # exponent for D calc, getting messy
+        D = multiply(D, exp(expon))  # Calc New D for next iteration
+        D = D / D.sum()
+        # calc training error of all classifiers, if this is 0 quit for loop early (use break)
+        aggClassEst += alpha * classEst
+        # print "aggClassEst: ",aggClassEst.T
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T, ones((m, 1)))
+        errorRate = aggErrors.sum() / m
+        print("total error: ", errorRate)
+        if errorRate == 0.0:
+            break
+    return weakClassArr, aggClassEst
+
+
+def adaClassify(datToClass, classifierArr):
+    dataMatrix = mat(datToClass)  # do stuff similar to last aggClassEst in adaBoostTrainDS
+    m = shape(dataMatrix)[0]
+    aggClassEst = mat(zeros((m, 1)))
+    for i in range(len(classifierArr)):
+        classEst = stumpClassify(dataMatrix, classifierArr[i]['dim'], classifierArr[i]['thresh'],
+                                 classifierArr[i]['ineq'])  # call stump classify
+        aggClassEst += classifierArr[i]['alpha'] * classEst
+        print(aggClassEst)
+    return sign(aggClassEst)
+
+
+def loadDataSet(fileName):  # general function to parse tab -delimited floats
+    numFeat = len(open(fileName).readline().split('\t'))  # get number of fields
+    dataMat = [];
+    labelMat = []
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = []
+        curLine = line.strip().split('\t')
+        for i in range(numFeat - 1):
+            lineArr.append(float(curLine[i]))
+        dataMat.append(lineArr)
+        labelMat.append(float(curLine[-1]))
+    return dataMat, labelMat
+
+
+def plotROC(predStrengths, classLabels):
+    import matplotlib.pyplot as plt
+    cur = (1.0, 1.0)  # cursor
+    ySum = 0.0  # variable to calculate AUC
+    numPosClas = sum(array(classLabels) == 1.0)
+    yStep = 1 / float(numPosClas);
+    xStep = 1 / float(len(classLabels) - numPosClas)
+    sortedIndicies = predStrengths.argsort()  # get sorted index, it's reverse
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+    # loop through all the values, drawing a line segment at each point
+    for index in sortedIndicies.tolist()[0]:
+        if classLabels[index] == 1.0:
+            delX = 0;
+            delY = yStep;
+        else:
+            delX = xStep;
+            delY = 0;
+            ySum += cur[1]
+        # draw line from cur to (cur[0]-delX,cur[1]-delY)
+        ax.plot([cur[0], cur[0] - delX], [cur[1], cur[1] - delY], c='b')
+        cur = (cur[0] - delX, cur[1] - delY)
+    ax.plot([0, 1], [0, 1], 'b--')
+    plt.xlabel('False positive rate');
+    plt.ylabel('True positive rate')
+    plt.title('ROC curve for AdaBoost horse colic detection system')
+    ax.axis([0, 1, 0, 1])
+    plt.show()
+    print("the Area Under the Curve is: ", ySum * xStep)
+
+
 if __name__ == '__main__':
-    dataMat, classLabels = loadSimpleData()
-    D = mat(ones((5, 1)) / 5)
-    buildStump(dataMat, classLabels, D)
+    # dataMat, classLabels = loadSimpleData()
+    # D = mat(ones((5, 1)) / 5)
+    # buildStump(dataMat, classLabels, D)
+
+    # classifierArray = adaBoostTrainDS(dataMat, classLabels, 9)
+    # print(classifierArray)
+
+    # classifierArr = adaBoostTrainDS(dataMat, classLabels, 30)
+    # adaClassify([0, 0], classifierArr)
+
+    dataArr, labelArr = loadDataSet('source_code/Ch07/horseColicTraining2.txt')
+    classifierArray, aggClassEst = adaBoostTrainDS(dataArr, labelArr, 10)
+    plotROC(aggClassEst.T, labelArr)
