1b8c39c5838e189b13c59ebc14fd9de5d297db3d
zhangwei <928241517@qq.com>
2017-12-12 11:49:14 +0800

    term frequency

--- principle_of_algorithm/chapter2/term_frequency2.5.py
+++ principle_of_algorithm/chapter2/term_frequency2.5.py
@@ -19,3 +19,21 @@ def writebunchobj(path, bunchobj):
     file_obj = open(path, "wb")
     pickle.dump(bunchobj, file_obj)
     file_obj.close()
+
+
+if __name__ == '__main__':
+    # 导入分词后的词向量Bunch对象
+    path = "train_word_bag/train_set.dat"
+    bunch = readbunchobj(path)
+    # 构建TF-IDF词向量空间对象
+    tfidfspace = Bunch(target_name=bunch.tar_get_name, label=bunch.label, filename=bunch.filename, tdm=[],
+                       vocabulary={})
+    # 使用TfidfVectorizer初始化向量空间模型
+    vectorizer = TfidfVectorizer(stop_words="stpwrdlst", sublinear_tf=True, max_df=0.5)
+    transformer = TfidfTransformer()
+    # 文本转为词频矩阵，单独保存字典文件
+    tfidfspace.tdm = vectorizer.fit_transform(bunch.contents)
+    tfidfspace.vocabulary = vectorizer.vocabulary_
+    # 创建词袋的持久化
+    space_path = "train_word_bag/tfdifspace.dat"
+    writebunchobj(space_path, tfidfspace)
