eb2abe37be0ad46441a643508e8b8542383f21f2
zhangwei <928241517@qq.com>
2017-12-27 11:11:28 +0800

    scikit learn pca

--- python_machine_learning/chapter5/PCA_1.py
+++ python_machine_learning/chapter5/PCA_1.py
@@ -4,6 +4,31 @@ import matplotlib.pyplot as plt
 
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import StandardScaler
+from matplotlib.colors import ListedColormap
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
+
 
 if __name__ == '__main__':
     df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
@@ -49,3 +74,27 @@ if __name__ == '__main__':
     plt.ylabel('PC 2')
     plt.legend(loc='lower left')
     plt.show()
+
+    ## 使用scikit-learn进行主成分分析
+    pca = PCA(n_components=2)
+    lr = LogisticRegression()
+    X_train_pca = pca.fit_transform(X_train_std)
+    X_test_pca = pca.transform(X_test_std)
+    lr.fit(X_train_pca, y_train)
+    plot_decision_regions(X_train_pca, y_train, classifier=lr)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.legend(loc='lower left')
+    plt.show()
+
+    ## 将各类分开
+    plot_decision_regions(X_test_pca, y_test, classifier=lr)
+    plt.xlabel('PC1')
+    plt.ylabel('PC2')
+    plt.legend(loc='lower left')
+    plt.show()
+
+
+    # pca = PCA(n_components=None)
+    # X_test_pca = pca.fit_transform(X_train_std)
+    # print(pca.explained_variance_ratio_)
