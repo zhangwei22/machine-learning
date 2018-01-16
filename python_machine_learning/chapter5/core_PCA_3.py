06c520a89a5ac9cd4388cc611a952fabdb23ecbd
zhangwei <928241517@qq.com>
2018-01-16 18:14:54 +0800

    new data point

--- python_machine_learning/chapter5/core_PCA_3.py
+++ python_machine_learning/chapter5/core_PCA_3.py
@@ -39,8 +39,12 @@ def rbf_kernel_pca(X, gamma, n_components):
     eigvals, eigvece = eigh(K)
 
     # Collect the top k eigenvectors(projected samples)
-    X_pc = np.column_stack((eigvece[:, -i] for i in range(1, n_components + 1)))
-    return X_pc
+    alphas = np.column_stack((eigvece[:, -i] for i in range(1, n_components + 1)))
+
+    # Collect the corresponding eigenvalues
+    lambdas = [eigvals[-i] for i in range(1, n_components + 1)]
+
+    return alphas, lambdas
 
 
 def show_datasets_moon(X, y):
@@ -88,7 +92,39 @@ def show_datasets_circular(X, y):
     plt.show()
 
 
+def standrad_pca_circular(X, y):
+    scikit_pca = PCA(n_components=2)
+    X_spca = scikit_pca.fit_transform(X)
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+    ax[0].scatter(X_spca[y == 0, 0], X_spca[y == 0, 1], color='red', marker='^', alpha=0.5)
+    ax[0].scatter(X_spca[y == 1, 0], X_spca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+    ax[1].scatter(X_spca[y == 0, 0], np.zeros((500, 1)) + 0.02, color='red', marker='^', alpha=0.5)
+    ax[1].scatter(X_spca[y == 1, 0], np.zeros((500, 1)) - 0.02, color='blue', marker='^', alpha=0.5)
+    ax[0].set_xlabel('PC1')
+    ax[0].set_ylabel('PC2')
+    ax[1].set_ylim([-1, 1])
+    ax[1].set_xlabel('PC1')
+    plt.show()
 
+
+def rbf_core_pca(X, y):
+    X_kpca = rbf_kernel_pca(X, gamma=15, n_components=2)
+    fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7, 3))
+    ax[0].scatter(X_kpca[y == 0, 0], X_kpca[y == 0, 1], color='red', marker='^', alpha=0.5)
+    ax[0].scatter(X_kpca[y == 1, 0], X_kpca[y == 1, 1], color='blue', marker='o', alpha=0.5)
+    ax[1].scatter(X_kpca[y == 0, 0], np.zeros((500, 1)) + 0.02, color='red', marker='^', alpha=0.5)
+    ax[1].scatter(X_kpca[y == 1, 0], np.zeros((500, 1)) - 0.02, color='blue', marker='^', alpha=0.5)
+    ax[0].set_xlabel('PC1')
+    ax[0].set_ylabel('PC2')
+    ax[1].set_ylim([-1, 1])
+    ax[1].set_xlabel('PC1')
+    plt.show()
+
+
+def project_X(x_new, X, gamma, alphas, lambdas):
+    pair_dist = np.array([np.sum((x_new - row) ** 2) for row in X])
+    k = np.exp(-gamma * pair_dist)
+    return k.dot(alphas / lambdas)
 
 
 if __name__ == '__main__':
@@ -101,8 +137,29 @@ if __name__ == '__main__':
     # # 用核pca函数实现
     # core_pca(X, y)
 
-    # 构造同心圆形数据
-    X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
-    # 展示数据集（同心圆形数据）
-    show_datasets_circular(X, y)
+    # # 构造同心圆形数据
+    # X, y = make_circles(n_samples=1000, random_state=123, noise=0.1, factor=0.2)
+    # # 展示数据集（同心圆形数据）
+    # show_datasets_circular(X, y)
+    # # 标准pca转换（同心圆形）
+    # standrad_pca_circular(X, y)
+    # # 基于RBF的核PCA
+    # rbf_core_pca(X, y)
 
+    X, y = make_moons(n_samples=100, random_state=123)
+    alphas, lambdas = rbf_kernel_pca(X, gamma=15, n_components=1)
+
+    X_new = X[25]
+    print(X_new)
+    X_proj = alphas[25]  # original projection
+    print(X_proj)
+
+    x_reproj = project_X(X_new, X, gamma=15, alphas=alphas, lambdas=lambdas)
+    print(x_reproj)
+
+    plt.scatter(alphas[y == 0, 0], np.zeros((50)), color='red', marker='^', alpha=0.5)
+    plt.scatter(alphas[y == 1, 0], np.zeros((50)), color='blue', marker='o', alpha=0.5)
+    plt.scatter(X_proj, 0, color='black', label='original projection of point X[25]', marker='^', s=100)
+    plt.scatter(x_reproj, 0, color='green', label='remapped point X[25]', marker='x', s=500)
+    plt.legend(scatterpoints=1)
+    plt.show()
