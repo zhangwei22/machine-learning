b8bc99375e5778f31535b38274120d4edad12220
zhangwei <928241517@qq.com>
2018-01-22 15:54:31 +0800

    modify a param

--- python_machine_learning/chapter8/imdb_dataset_1.py
+++ python_machine_learning/chapter8/imdb_dataset_1.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
     pbar = pyprind.ProgBar(50000)
     labels = {'pos': 1, 'neg': 0}
     df = pd.DataFrame()
-    for s in ('tree', 'train'):
+    for s in ('test', 'train'):
         for l in ('pos', 'neg'):
-            path = './aclImdb/%s/%s' % (s, l)
+            path = '/Users/zhangwei/Desktop/aclImdb/%s/%s' % (s, l)
             for file in os.listdir(path):
                 with open(os.path.join(path, file), 'r') as infile:
                     txt = infile.read()
